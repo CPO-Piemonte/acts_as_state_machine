0b6271af56f4a5b6521f7669b80a44e902d7571c
Francesco <francescob@gmail.com>
2022-03-14 11:55:06 +0100

    provo a rendere questa gemma compatibile con versioni più recenti di ruby

--- lib/acts_as_state_machine.rb
+++ lib/acts_as_state_machine.rb
@@ -117,17 +117,26 @@ module ScottBarron                   #:nodoc:
             include InstanceMethods
 
             raise NoInitialState unless options[:initial]
+            class_attribute :states
+            states = {}
+            class_attribute :initial_state
+            initial_state = options[:initial]
+            class_attribute :transition_table, {}
+            transition_table = {}
+            class_attribute :event_table
+            event_table = {}
+            class_attribute :state_column
+            state_column = options[:column] || 'state'
+            #write_inheritable_attribute :states, {}
+            #write_inheritable_attribute :initial_state, options[:initial]
+            #write_inheritable_attribute :transition_table, {}
+            #write_inheritable_attribute :event_table, {}
+            #write_inheritable_attribute :state_column, options[:column] || 'state'
 
-            write_inheritable_attribute :states, {}
-            write_inheritable_attribute :initial_state, options[:initial]
-            write_inheritable_attribute :transition_table, {}
-            write_inheritable_attribute :event_table, {}
-            write_inheritable_attribute :state_column, options[:column] || 'state'
-
-            class_inheritable_reader    :initial_state
-            class_inheritable_reader    :state_column
-            class_inheritable_reader    :transition_table
-            class_inheritable_reader    :event_table
+            #class_inheritable_reader    :initial_state
+            #class_inheritable_reader    :state_column
+            #class_inheritable_reader    :transition_table
+            #class_inheritable_reader    :event_table
 
             before_create               :set_initial_state
             after_create                :run_initial_state_actions
